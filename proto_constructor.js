1d2d676847651716cd40aac570fe4c51ee191b34
PSY <p.s.84@mail.ru>
2019-11-06 23:23:58 +0300

    add task 2

--- proto_constructor.js
+++ proto_constructor.js
@@ -1,6 +1,7 @@
-// Задание № 1
-
 'use strict';
+
+// Задание № 1
+console.log(`===== Задание № 1 =====`); 
 
 var positions = [
   {
@@ -68,7 +69,7 @@ items[2].hold(1);
 for (let item of items) {
   console.log(`Товар ${item}`);
 }
-console.log(`======================================================================`);
+console.log(`=====`);
 
 items[0].unhold(1);
 items[1].unhold(2);
@@ -80,5 +81,39 @@ for (let item of items) {
 }
 
 // Задание № 2
+console.log(`===== Задание № 2 =====`); 
+
+
+const config = {
+  get() {
+    return this.price - (this.price * this.discount / 100);
+  },
+
+  set(amount) {
+    try {
+      if (amount > this.price) {
+        throw new Error ('Финальная стоимость превышает обычную!');
+      }
+
+      this.discount = ((this.price - amount) / this.price * 100).toFixed(1);
+
+    } catch(e) {
+      console.log(e.message);
+    }
+  }
+}
+
+positions.forEach((item) => {
+  Object.defineProperty(item, 'finalPrice', config)
+});
+
+console.log(positions[0].finalPrice); // 9300
+positions[2].finalPrice = 28500;
+console.log(positions[2].discount); // 50
+positions[2].finalPrice = 58500; // Финальная стоимость превышает обычную!
+console.log(positions[2].discount); // 50
+console.log(positions[2].finalPrice); // 28500
+positions[2].finalPrice = 56700;
+console.log(positions[2].discount); // 0.5
 
 
